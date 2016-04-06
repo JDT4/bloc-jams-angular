3fd7e5c6805b087008dfe05ab2c3240635d7490b
JDT4 <incipientviolet@gmail.com>
2016-04-06 21:17:26 +0100

    error-fixtures not defined

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,16 +1,19 @@
 (function () {
 	function AlbumCtrl() {
-		var albumData = angular.copy(albumPicasso);
+		var albumData = fixtures.getAlbum()
+		return albumData;
+		//this.albumData = fixtures.getAlbum();
+		/*var albumData = angular.copy(albumPicasso);*/
 		this.name = albumData.name;
 		this.artist = albumData.artist;
 		this.label = albumData.label;
 		this.year = albumData.year;
 		this.albumArtUrl = albumData.albumArtUrl;
 		this.songs = albumData.songs;
-
 	}
 
 	angular
 		.module('blocJams')
-		.controller('AlbumCtrl', AlbumCtrl);
+		//	.controller('AlbumCtrl', [AlbumCtrl]);
+		.controller('AlbumCtrl', ['fixtures', AlbumCtrl]);
 })();
