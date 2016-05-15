acddd203a8187b012621fc3a9cde5b47cded6dc1
JDT4 <incipientviolet@gmail.com>
2016-05-15 17:38:38 +0100

    assignment work finished

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -14,12 +14,22 @@
  			templateUrl: '/templates/directives/seek_bar.html',
  			replace: true,
  			restrict: 'E',
- 			scope: {},
+ 			scope: {
+ 				onChange: '&'
+ 			},
  			link: function (scope, element, attributes) {
  				scope.value = 0;
  				scope.max = 100;
 
  				var seekBar = $(element);
+
+ 				attributes.$observe('value', function (newValue) {
+ 					scope.value = newValue;
+ 				});
+
+ 				attributes.$observe('max', function (newValue) {
+ 					scope.max = newValue;
+ 				});
 
  				var percentString = function () {
  					var value = scope.value;
@@ -33,15 +43,18 @@
  						width: percentString()
  					};
  				};
+
  				scope.onClickSeekBar = function (event) {
  					var percent = calculatePercent(seekBar, event);
  					scope.value = percent * scope.max;
+ 					notifyOnChange(scope.value);
  				};
  				scope.trackThumb = function () {
  					$document.bind('mousemove.thumb', function (event) {
  						var percent = calculatePercent(seekBar, event);
  						scope.$apply(function () {
  							scope.value = percent * scope.max;
+ 							notifyOnChange(scope.value);
  						});
  					});
 
@@ -50,10 +63,17 @@
  						$document.unbind('mouseup.thumb');
  					});
  				};
+ 				var notifyOnChange = function (newValue) {
+ 					if (typeof scope.onChange === 'function') {
+ 						scope.onChange({
+ 							value: newValue
+ 						});
+ 					}
+ 				};
  			}
  		};
  	}
  	angular
  		.module('blocJams')
- 		.directive('seekBar', ['$document',seekBar]);
+ 		.directive('seekBar', ['$document', seekBar]);
  })();
