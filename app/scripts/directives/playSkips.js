594eecda8db4d22410c0b4fdc8a1ea84d747011c
JDT4 <incipientviolet@gmail.com>
2016-07-06 22:17:32 +0100

    chart directive added

--- app/scripts/directives/playSkips.js
+++ app/scripts/directives/playSkips.js
@@ -1,14 +1,27 @@
  (function () {
- 	function playSkips($document) {
+ 	function playSkips() {
  		return {
- 			templateUrl: '/templates/directives/seek_bar.html',
+ 			template: "<canvas width='300px' height='100%'></canvas>",
  			restrict: 'E',
- 			$scope.graph = {
- 				//Data goes here
+ 			replace: true,
+ 			scope: {
+ 				data: "="
+ 			},
+ 			link: function (scope, element, attributes) {
+ 				//console.log(scope);
+ 				//console.log(element);
+ 				//console.log(attributes);
+ 				console.log(JSON.stringify(scope.data));
+
+ 				var myChart = new Chart(element, {
+ 					type: "doughnut",
+ 					data: scope.data,
+ 					options: {},
+ 				});
  			}
  		};
  	}
  	angular
  		.module('blocJams')
- 		.directive('playSkips', ['$document', playSkips]);
+ 		.directive('playSkips', [playSkips]);
  })();
